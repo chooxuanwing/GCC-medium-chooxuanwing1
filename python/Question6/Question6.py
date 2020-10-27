e9068b2e37ecf145d7ee327b2a2f9932399f54ba
CS-CodingChallenge <44158926+CS-CodingChallenge@users.noreply.github.com>
2020-10-27 17:51:03 -0400

    Commited by GCC-Challenge-2020

--- python/Question6/Question6.py
+++ python/Question6/Question6.py
@@ -2,6 +2,23 @@ import math
 # You may change this function parameters
 def encrypt(words):
     # Participants code will be here
+    
+    wordsFormatted = words.replace(" ","")
+    print(wordsFormatted)
+    stringLength = len(wordsFormatted)
+    sqrt = pow(stringLength,0.5)
+    lowBound = math.floor(sqrt)
+    upBound = math.ceil(sqrt)
+    checkBox = lowBound*upBound
+    
+    if (checkBox < stringLength):
+        lowBound += 1
+    
+    out = ''
+    
+    for i in range(0,stringLength//lowBound):
+        out += wordsFormatted[i*upBound]
+        print(out)
     return " "
 
 
