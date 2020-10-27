270bc0b629e4e955c2900d541d26c68575072018
Wing <chooxuanwing@gmail.com>
2020-10-27 19:03:21 +0000

    solved4

--- python/Question4/Question4.py
+++ python/Question4/Question4.py
@@ -1,6 +1,19 @@
+# import sys
 # Participants may update the following function parameters
 def maximumExpectedMoney(noOfTradesAvailable, maximumTradesAllowed,p,x,y):
-    return -1
+
+    totalTrades = []
+    sum=0
+    for i in range(0,noOfTradesAvailable):
+        totalTrades.append(p[i]*x[i]-(1-p[i])*y[i])
+    totalTrades = sorted(totalTrades, reverse=True)
+    print(totalTrades)
+
+    for j in range(0,maximumTradesAllowed):
+        sum+=totalTrades[j]
+    print(sum)
+
+    return sum
 
 def main():
     # This part may require participants to fill in as well.
@@ -9,6 +22,12 @@ def main():
     x = list(map(float, input().split()))
     y = list(map(float, input().split()))
 
+    # noOfTradesAvailable = 4
+    # maximumTradesAllowed = 2
+    # p=[0.5 ,0.5 ,0.5 ,0.5]
+    # x=[4.00, 1.00, 2.00, 3.00]
+    # y=[4.00 ,0.50 ,1.00 ,1.00]
+    
     # Participants may update the following function parameters
     answer = maximumExpectedMoney(noOfTradesAvailable, maximumTradesAllowed,p,x,y)
     # Do not remove below line
